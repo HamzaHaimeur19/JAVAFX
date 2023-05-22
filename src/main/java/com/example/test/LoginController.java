361e31b66dcf3f91964df0ce4eaea6767bbae6dd
Hamza.Haimeur <HamzaHaimeur19@Gmail.com>
2023-05-22 11:35:55 +0100

    n design

--- src/main/java/com/example/test/LoginController.java
+++ src/main/java/com/example/test/LoginController.java
@@ -29,10 +29,10 @@ public class LoginController {
         String password = passwordField.getText();
 
         try {
-            // Establish a connection to the MySQL database
+            // etabli
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/equipejoueur", "root", "");
 
-            // Create a SQL query to check if the entered credentials exist in the database
+            // requette sql
             String query = "SELECT * FROM user WHERE email = ? AND password = ?";
             PreparedStatement statement = connection.prepareStatement(query);
             statement.setString(1, email);
@@ -43,35 +43,23 @@ public class LoginController {
             if (resultSet.next()) {
                 messageLabel.setText("Login successful!");
 
-                // Close the login window
+                // fermer fenetre de login
                 Stage stage = (Stage) emailField.getScene().getWindow();
                 stage.close();
 
-                // Open the equipe view
+                // ouvrir equipe view
                 HelloApplication.navigateToEquipe();
             } else {
                 // Message d'erreur si aucun utilisateur n'est trouvé
                 messageLabel.setText("Invalid username or password");
             }
 
-            // Close the database resources
+            // fermer resources
             resultSet.close();
             statement.close();
             connection.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
-    @FXML
-    private void deconnexion() {
-        // Clear the email and password fields
-        emailField.clear();
-        passwordField.clear();
-
-        // Reset the message label
-        messageLabel.setText("");
-
-        // Navigate back to the login view
-        HelloApplication.navigateToLogin();
     }
 }
